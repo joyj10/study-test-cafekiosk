7a0bed39b3584f69afbfc50b39a22e5ef338c59b
dev_one <micool1030@gmail.com>
2023-11-11 00:04:50 +0900

    # 아메리카노, 키오스크 단위테스트 개발

---
+++ src/test/java/sample/cafekiosk/spring/unit/beverage/AmericanoTest.java
@@ -0,0 +1,41 @@
+package sample.cafekiosk.spring.unit.beverage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * AmericanoTest
+ * <pre>
+ * Describe here
+ * </pre>
+ *
+ * @version 1.0,
+ */
+class AmericanoTest {
+
+    @Test
+    void getName() {
+        Americano americano = new Americano();
+        String name = americano.getName();
+
+        // JUnit API
+        assertEquals("아메리카노", name);
+
+        // assertJ API
+        assertThat(name).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void getPrice() {
+        Americano americano = new Americano();
+        int price = americano.getPrice();
+
+        // JUnit API
+        assertEquals(4500, price);
+
+        // assertJ API
+        assertThat(price).isEqualTo(4500);
+    }
+}
