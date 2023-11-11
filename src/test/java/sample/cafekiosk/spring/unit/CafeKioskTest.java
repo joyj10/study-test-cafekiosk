88d6820136b131042a1110b404c862aa9807fc7f
dev_one <micool1030@gmail.com>
2023-11-11 23:16:06 +0900

    # DisplayName 추가

--- src/test/java/sample/cafekiosk/spring/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/spring/unit/CafeKioskTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.unit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.spring.unit.beverage.Americano;
 import sample.cafekiosk.spring.unit.beverage.Latte;
@@ -22,6 +23,8 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+//    @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -79,6 +82,24 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
+    @Test
+    void calculateTotalPrice() {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+
+        // when
+        int totalPrice = cafeKiosk.calculateTotalPrice();
+
+        // then
+        assertThat(totalPrice).isEqualTo(8500);
+    }
+
     @Test
     void createOrder() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -89,7 +110,6 @@ class CafeKioskTest {
         assertThat(order.getBeverages()).hasSize(1);
         assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
     }
-
 
     @Test
     void createOrderWithCurrentTime() {
