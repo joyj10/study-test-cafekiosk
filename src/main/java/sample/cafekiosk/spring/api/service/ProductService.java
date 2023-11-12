128f2db586b7e3f2bfe37ff3aa67e51b046dd05e
dev_one <micool1030@gmail.com>
2023-11-12 12:37:30 +0900

    # 원하는 판매상태의 상품 조회 개발 및 테스트 코드 구현

---
+++ src/main/java/sample/cafekiosk/spring/api/service/ProductService.java
@@ -0,0 +1,28 @@
+package sample.cafekiosk.spring.api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.service.response.ProductResponse;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public List<ProductResponse> getSellingProducts() {
+        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+
+        return products.stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+}
