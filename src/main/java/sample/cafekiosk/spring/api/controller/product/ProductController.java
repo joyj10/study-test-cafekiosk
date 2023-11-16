f32aa9807c9f73fde1dc029a89b9d5b8ff54637f
dev_one <micool1030@gmail.com>
2023-11-16 22:48:34 +0900

    [feat] ApiResponse 추가 - api 리턴 객체 공통화

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,10 +1,12 @@
 package sample.cafekiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.controller.product.dto.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
@@ -19,8 +21,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public ProductResponse crateProduct(@Valid @RequestBody ProductCreateRequest request) {
-        return productService.createProduct(request);
+    public ApiResponse<ProductResponse> crateProduct(@Valid @RequestBody ProductCreateRequest request) {
+        return ApiResponse.ok(productService.createProduct(request));
     }
 
     @GetMapping("/api/v1/products/selling")
